70b7fd1edc9634984576ddb9638e20a3ee5bbc15
Stephen Harrison <stephen@redleopard.org>
2021-02-15 23:12:14 +0000

    Add feedback for selected clip ID

--- index.js
+++ index.js
@@ -489,7 +489,7 @@ class instance extends instance_skel {
 						type: 'dropdown',
 						label: 'Slot (1/2)',
 						id: 'slot',
-						default: "1",
+						default: 1,
 						choices: [
 							{ id: 1, label: 'Slot 1' },
 							{ id: 2, label: 'Slot 2' }
@@ -911,22 +911,28 @@ class instance extends instance_skel {
 				}
 			}
 		}
-		feedbacks['slot_status'] = {
-			label: 'Slot/disk status',
-			description: 'Based on disk status, change colors of the bank',
+		feedbacks['transport_clip'] = {
+			label: 'Active clip',
+			description: 'Set the colour based on the which clip is active',
 			options: [
 				{
-					type:    'dropdown',
-					label:   'Disk Status',
-					id:      'status',
-					choices: this.CHOICES_SLOTSTATUS
-				},
-				{
 					type:    'textinput',
-					label:   'Slot Id',
-					id:      'slotId',
+					label:   'Clip Id',
+					id:      'clipID',
 					default: 1,
 					regex:   this.REGEX_SIGNED_NUMBER
+				},
+				{
+					type:    'dropdown',
+					label:   'Slot Id',
+					id:      'slotID',
+					choices: [
+						{ id: 'either',  label: 'Either'  },
+						{ id: 1 , label: 'Slot 1' },
+						{ id: 2 , label: 'Slot 2' }
+					],
+					default: 'either',
+					regex:   this.REGEX_SOMETHING
 				},
 				{
 					type:    'colorpicker',
@@ -942,8 +948,10 @@ class instance extends instance_skel {
 				}
 			],
 			callback: ({ options }, bank) => {
-				const slot = this.slotInfo[options.slotId]
-				if (slot && slot.status === options.status) {
+				console.log('Clip: %O - Clip %O', options.clipID, this.transportInfo.clipId)
+				console.log('Slot: %O - Slot %O', options.slotID, this.transportInfo.slotId)
+				if ((options.slotID == 'either' && options.clipID == this.transportInfo.clipId) ||
+						(options.slotID == this.transportInfo.slotId && options.clipID == this.transportInfo.clipId)) {
 					return {
 						color:   options.fg,
 						bgcolor: options.bg
@@ -977,6 +985,46 @@ class instance extends instance_skel {
 			],
 			callback: ({ options }, bank) => {
 				if (options.setting == this.transportInfo.slotId) {
+					return {
+						color:   options.fg,
+						bgcolor: options.bg
+					};
+				}
+			}
+		}
+		feedbacks['slot_status'] = {
+			label: 'Slot/disk status',
+			description: 'Based on disk status, change colors of the bank',
+			options: [
+				{
+					type:    'dropdown',
+					label:   'Disk Status',
+					id:      'status',
+					choices: this.CHOICES_SLOTSTATUS
+				},
+				{
+					type:    'textinput',
+					label:   'Slot Id',
+					id:      'slotId',
+					default: 1,
+					regex:   this.REGEX_SIGNED_NUMBER
+				},
+				{
+					type:    'colorpicker',
+					label:   'Foreground color',
+					id:      'fg',
+					default: this.rgb(255,255,255)
+				},
+				{
+					type:    'colorpicker',
+					label:   'Background color',
+					id:      'bg',
+					default: this.rgb(0,0,0)
+				}
+			],
+			callback: ({ options }, bank) => {
+				const slot = this.slotInfo[options.slotId]
+				if (slot && slot.status === options.status) {
 					return {
 						color:   options.fg,
 						bgcolor: options.bg
@@ -1274,7 +1322,7 @@ class instance extends instance_skel {
 				this.initVariables();
 			})
 
-			this.hyperDeck.on('notify.transport', res => {
+			this.hyperDeck.on('notify.transport', async res => {
 				this.log('debug', 'Transport Status Changed')
 				for (var id in res) {
 					if (res[id] !== undefined) {
@@ -1288,7 +1336,7 @@ class instance extends instance_skel {
 			this.hyperDeck.connect(this.config.host, this.config.port)
 			
 			// hyperdeck-connection debug tool
-			//this.hyperDeck.DEBUG = true;
+			this.hyperDeck.DEBUG = true;
 		}
 	}
 

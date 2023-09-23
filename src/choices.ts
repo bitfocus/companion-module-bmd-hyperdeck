d5a9835bb4121c8987a662087268ce273b923431
Julian Waller <git@julusian.co.uk>
2023-09-23 21:10:05 +0100

    fix: make some choices be enum backed

--- src/choices.ts
+++ src/choices.ts
@@ -82,54 +82,84 @@ export const CHOICES_REMOTESTATUS: DropdownChoice[] = [
 	{ id: DropdownChoiceFalse, label: 'Disabled' },
 ]
 
+export enum AudioInputType {
+	Embedded = 'embedded',
+	XLR = 'XLR',
+	RCA = 'RCA',
+}
+
 const CONFIG_AUDIOINPUTS: Record<string, DropdownChoice | undefined> = {
-	embedded: { id: 'embedded', label: 'Embedded' },
-	XLR: { id: 'XLR', label: 'XLR' },
-	RCA: { id: 'RCA', label: 'RCA' },
+	embedded: { id: AudioInputType.Embedded, label: 'Embedded' },
+	XLR: { id: AudioInputType.XLR, label: 'XLR' },
+	RCA: { id: AudioInputType.RCA, label: 'RCA' },
+}
+
+export enum VideoInputType {
+	SDI = 'SDI',
+	HDMI = 'HDMI',
+	Component = 'component',
+	Composite = 'composite',
+	Optical = 'optical',
 }
 
 const CONFIG_VIDEOINPUTS: Record<string, DropdownChoice | undefined> = {
-	SDI: { id: 'SDI', label: 'SDI' },
-	HDMI: { id: 'HDMI', label: 'HDMI' },
-	component: { id: 'component', label: 'Component' },
-	composite: { id: 'composite', label: 'Composite' },
-	optical: { id: 'optical', label: 'Optical' },
+	SDI: { id: VideoInputType.SDI, label: 'SDI' },
+	HDMI: { id: VideoInputType.HDMI, label: 'HDMI' },
+	component: { id: VideoInputType.Component, label: 'Component' },
+	composite: { id: VideoInputType.Composite, label: 'Composite' },
+	optical: { id: VideoInputType.Optical, label: 'Optical' },
 }
 
 export interface FormatDropdownChoice extends DropdownChoice {
-	family: string
+	family: FileFormatFamily
+}
+
+export enum FileFormatFamily {
+	Uncompressed = 'uncompressed',
+	ProRes = 'prores',
+	Proxy = 'proxy',
+	DNx = 'DNx',
+	DNxHD220 = 'DNxHD220',
+	DNxHR_HQX = 'DNxHR_HQX',
+	DNxHR_SQ = 'DNxHR_SQ',
+	DNxHR_LB = 'DNxHR_LB',
+	H264_SDI = 'H.264_SDI',
+	H264 = 'H.264',
+	H265 = 'H.265',
+	H264_5 = 'H.264/5',
+	Teleprompter = 'teleprompter',
 }
 
 export const CONFIG_FILEFORMATS: FormatDropdownChoice[] = [
-	{ id: 'QuickTimeUncompressed', label: 'QuickTime Uncompressed', family: 'uncompressed' },
-	{ id: 'QuickTimeProResHQ', label: 'QuickTime ProRes HQ', family: 'prores' },
-	{ id: 'QuickTimeProRes', label: 'QuickTime ProRes', family: 'prores' },
-	{ id: 'QuickTimeProResLT', label: 'QuickTime ProRes LT', family: 'prores' },
-	{ id: 'QuickTimeProResProxy', label: 'QuickTime ProRes Proxy', family: 'proxy' },
-	{ id: 'QuickTimeDNxHD45', label: 'DNxHD 45 QT', family: 'DNx' },
-	{ id: 'DNxHD45', label: 'DNxHD 45 MXF', family: 'DNx' },
-	{ id: 'QuickTimeDNxHD145', label: 'DNxHD 145 QT', family: 'DNx' },
-	{ id: 'DNxHD145', label: 'DNxHD 145 MXF', family: 'DNx' },
-	{ id: 'QuickTimeDNxHD220', label: 'DNxHD 220 QT', family: 'DNxHD220' },
-	{ id: 'DNxHD220', label: 'MXF DNxHD 220', family: 'DNxHD220' },
-	{ id: 'QuickTimeDNxHR_HQX', label: 'DNxHR HQX QT', family: 'DNxHR_HQX' },
-	{ id: 'DNxHR_HQX', label: 'DNxHR HQX MXF', family: 'DNxHR_HQX' },
-	{ id: 'QuickTimeDNxHR_SQ', label: 'DNxHR SQ QT', family: 'DNxHR_SQ' },
-	{ id: 'DNxHR_SQ', label: 'DNxHR SQ MXF', family: 'DNxHR_SQ' },
-	{ id: 'QuickTimeDNxHR_LB', label: 'DNxHR LB QT', family: 'DNxHR_LB' },
-	{ id: 'DNxHR_LB', label: 'DNxHR LB MXF', family: 'DNxHR_LB' },
-	{ id: 'H.264High10_422', label: 'H.264 SDI', family: 'H.264_SDI' },
-	{ id: 'H.264High', label: 'H.264 High', family: 'H.264' },
-	{ id: 'H.264Medium', label: 'H.264 Medium', family: 'H.264' },
-	{ id: 'H.264Low', label: 'H.264 Low', family: 'H.264' },
-	{ id: 'H.265High', label: 'H.265 High', family: 'H.265' },
-	{ id: 'H.265Medium', label: 'H.265 Medium', family: 'H.265' },
-	{ id: 'H.265Low', label: 'H.265 Low', family: 'H.265' },
-	{ id: 'H.264High10_422', label: 'H.264/5 SDI', family: 'H.264/5' },
-	{ id: 'H.264High', label: 'H.264/5 High', family: 'H.264/5' },
-	{ id: 'H.264Medium', label: 'H.264/5 Medium', family: 'H.264/5' },
-	{ id: 'H.264Low', label: 'H.264/5 Low', family: 'H.264/5' },
-	{ id: 'Teleprompter', label: 'Teleprompter', family: 'teleprompter' },
+	{ id: 'QuickTimeUncompressed', label: 'QuickTime Uncompressed', family: FileFormatFamily.Uncompressed },
+	{ id: 'QuickTimeProResHQ', label: 'QuickTime ProRes HQ', family: FileFormatFamily.ProRes },
+	{ id: 'QuickTimeProRes', label: 'QuickTime ProRes', family: FileFormatFamily.ProRes },
+	{ id: 'QuickTimeProResLT', label: 'QuickTime ProRes LT', family: FileFormatFamily.ProRes },
+	{ id: 'QuickTimeProResProxy', label: 'QuickTime ProRes Proxy', family: FileFormatFamily.Proxy },
+	{ id: 'QuickTimeDNxHD45', label: 'DNxHD 45 QT', family: FileFormatFamily.DNx },
+	{ id: 'DNxHD45', label: 'DNxHD 45 MXF', family: FileFormatFamily.DNx },
+	{ id: 'QuickTimeDNxHD145', label: 'DNxHD 145 QT', family: FileFormatFamily.DNx },
+	{ id: 'DNxHD145', label: 'DNxHD 145 MXF', family: FileFormatFamily.DNx },
+	{ id: 'QuickTimeDNxHD220', label: 'DNxHD 220 QT', family: FileFormatFamily.DNxHD220 },
+	{ id: 'DNxHD220', label: 'MXF DNxHD 220', family: FileFormatFamily.DNxHD220 },
+	{ id: 'QuickTimeDNxHR_HQX', label: 'DNxHR HQX QT', family: FileFormatFamily.DNxHR_HQX },
+	{ id: 'DNxHR_HQX', label: 'DNxHR HQX MXF', family: FileFormatFamily.DNxHR_HQX },
+	{ id: 'QuickTimeDNxHR_SQ', label: 'DNxHR SQ QT', family: FileFormatFamily.DNxHR_SQ },
+	{ id: 'DNxHR_SQ', label: 'DNxHR SQ MXF', family: FileFormatFamily.DNxHR_SQ },
+	{ id: 'QuickTimeDNxHR_LB', label: 'DNxHR LB QT', family: FileFormatFamily.DNxHR_LB },
+	{ id: 'DNxHR_LB', label: 'DNxHR LB MXF', family: FileFormatFamily.DNxHR_LB },
+	{ id: 'H.264High10_422', label: 'H.264 SDI', family: FileFormatFamily.H264_SDI },
+	{ id: 'H.264High', label: 'H.264 High', family: FileFormatFamily.H264 },
+	{ id: 'H.264Medium', label: 'H.264 Medium', family: FileFormatFamily.H264 },
+	{ id: 'H.264Low', label: 'H.264 Low', family: FileFormatFamily.H264 },
+	{ id: 'H.265High', label: 'H.265 High', family: FileFormatFamily.H265 },
+	{ id: 'H.265Medium', label: 'H.265 Medium', family: FileFormatFamily.H265 },
+	{ id: 'H.265Low', label: 'H.265 Low', family: FileFormatFamily.H265 },
+	{ id: 'H.264High10_422', label: 'H.264/5 SDI', family: FileFormatFamily.H264_5 },
+	{ id: 'H.264High', label: 'H.264/5 High', family: FileFormatFamily.H264_5 },
+	{ id: 'H.264Medium', label: 'H.264/5 Medium', family: FileFormatFamily.H264_5 },
+	{ id: 'H.264Low', label: 'H.264/5 Low', family: FileFormatFamily.H264_5 },
+	{ id: 'Teleprompter', label: 'Teleprompter', family: FileFormatFamily.Teleprompter },
 ]
 
 const CONFIG_SLOT_LABELS: Record<string, DropdownChoice[] | undefined> = {
